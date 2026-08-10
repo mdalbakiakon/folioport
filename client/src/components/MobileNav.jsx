0c7db3501fed0b570f7739e2b6021012d514ee52
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-08-10 14:45:04 +0600

    fix: mobile navbar logo loading delay addressed

--- client/src/components/MobileNav.jsx
+++ client/src/components/MobileNav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const MobileNav = ({
@@ -10,6 +10,14 @@ const MobileNav = ({
   handleMobileMenu,
   theme,
 }) => {
+  
+  const [isVisible, setIsVisible] = useState(false);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => setIsVisible(true), 50);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <section className="w-full bg-(--bg-main) text-(--txt-main) flex justify-center items-center fixed top-19.5 left-0">
       <nav className="flex-col justify-center items-center gap-4.5 text-(--txt-sec) max-[529px]:flex relative z-20">
@@ -50,21 +58,31 @@ const MobileNav = ({
         </Link>
       </nav>
 
-      <img
-        src="/logo-light.svg"
-        alt="Portfolio Logo"
-        className={`absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-2/3 py-1.25 z-10 transition-opacity blur-sm duration-50 select-none ease-in-out ${
-          theme === "dark" ? "opacity-0" : "opacity-85"
+      <div
+        className={`w-full h-full absolute top-0 left-0 transition-opacity duration-750 ease-out ${
+          isVisible ? "opacity-100" : "opacity-0"
         }`}
-      />
+      >
+        <img
+          src="/logo-light.svg"
+          alt="Md. Al Baki Akon - Portfolio Logo"
+          loading="eager"
+          fetchPriority="high"
+          className={`absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-2/3 py-1.25 z-10 transition-opacity blur-sm duration-50 select-none ease-in-out ${
+            theme === "dark" ? "opacity-0" : "opacity-85"
+          }`}
+        />
 
-      <img
-        src="/logo-dark.svg"
-        alt="Portfolio Logo"
-        className={`absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-2/3 py-1.25 z-10 transition-opacity blur-sm duration-50 select-none ease-in-out ${
-          theme === "dark" ? "opacity-50" : "opacity-0"
-        }`}
-      />
+        <img
+          src="/logo-dark.svg"
+          alt="d. Al Baki Akon - Portfolio Logo"
+          loading="eager"
+          fetchPriority="high"
+          className={`absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-2/3 py-1.25 z-10 transition-opacity blur-sm duration-50 select-none ease-in-out ${
+            theme === "dark" ? "opacity-50" : "opacity-0"
+          }`}
+        />
+      </div>
     </section>
   );
 };
